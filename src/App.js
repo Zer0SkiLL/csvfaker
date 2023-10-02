c641eae0c63fc0ae3de1fa576930833ae499069f
Widmer Reto <reto.widmer@finstar.ch>
2023-10-02 11:42:27 +0200

    navigate rediredt empty to csv

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import AnonymizationPage from './components/AnonymizationPage';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <br />
         <Routes>
           <Route exact path="/csv" element={<AnonymizationPage />} />
+          <Route exact path='/' element={<Navigate to="/csv" />} />
           {/* <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} /> */}
         </Routes>
